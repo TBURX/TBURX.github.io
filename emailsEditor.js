4cfde7b17d9d091a5c15e2a13cffbeb57558f8fd
Vitaly <subbot.v@gmail.com>
2016-03-25 23:48:46 +0500

    paste fix

--- emailsEditor.js
+++ emailsEditor.js
@@ -1,70 +1,91 @@
-var app = angular.module("editEmailsApp", []);
+angular.module("editEmailsApp", [])
+    .directive("emailsEditor", function() {
+        return {
+            restrict: 'E'
+            ,
+            link: function($scope, $elem, $attrs){
+                
+                console.log("Hello, i'm emails-edit directive");
+                
+                $scope.emailsString = "";
+                $scope.eMails = [];
+                $scope.pasted = false;
 
-app.directive("emailsEditor", function() {
-    return {
-        restrict: 'E'
-        ,
-        link: function($scope, $elem, $attrs){
-            console.log("Hello, i'm emails-edit directive");
-            $scope.emailsString="";
-            $scope.eMails = [];
-            $scope.getEmailsCount = function(){
-                alert("e-mails count is: " + $scope.eMails.length);
-            };
-            $scope.addEmail = function(){
-                var charSet="qwertyuiopasdfghjklzxcvbnm";
-                var sample="";
-                var len=Math.floor(Math.random()*8+3);
-                for(i=0; i<len; i++)
+                $scope.getEmailsCount = function(){
+                    alert("e-mails count is: " + $scope.eMails.length);
+                };
+
+                $scope.addEmail = function(){
+                    $scope.emailsString=genEmail();
+                    addEmails($scope);        
+                };
+
+                $scope.change = function(){
+                    if($scope.emailsString.indexOf(",")>=0 || $scope.pasted)
                     {
-                        sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                        addEmails($scope);
+                        $scope.pasted = false;
                     }
-                sample+="@";
-                len=Math.floor(Math.random()*8+3);
-                for(i=0; i<len; i++)
+                };
+
+                $scope.keyUp = function($event){
+                    if($event.keyCode==13)
                     {
-                        sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                        addEmails($scope);
                     }
-                sample+=".";
-                len=Math.floor(Math.random()*2+2);
-                for(i=0; i<len; i++)
-                    {
-                        sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
-                    }
-                $scope.emailsString=sample;
-                addEmails($scope);        
-            };
-            $scope.change = function(){
-                if($scope.emailsString.indexOf(",")>=0 )
-                {
+                };
+
+                $scope.blur = function(){
                     addEmails($scope);
-                }
-            };
-            $scope.keyUp = function($event){
-                if($event.keyCode==13)
-                {
-                    addEmails($scope);
-                }
-            };
-            $scope.blur = function(){
-                    addEmails($scope);
-            };
-            $scope.remove = function($index){
-                $scope.eMails.splice($index,1);
-            };
-            $scope.focus = function(){
-                ($elem)[0].getElementsByTagName("input")[0].focus();
-            };
+                };
+                
+                $scope.paste = function(){
+                    $scope.pasted = true;              
+                };
+
+                $scope.remove = function($index){
+                    $scope.eMails.splice($index,1);
+                };
+
+                $scope.focus = function(){
+                    ($elem)[0].getElementsByTagName("input")[0].focus();
+                };
+            }
+            ,
+            templateUrl : "emailsEditor.html"
+            ,
+            scope: {
+                addEmail: "=addEmail",
+                getEmailsCount: "=getEmailsCount",
+                emailsString: "@emailsString"
+            }
+        };
+    })
+    .controller("editEmailsController",function($scope){
+    })
+
+function genEmail(){    
+    var charSet="qwertyuiopasdfghjklzxcvbnm";
+    var sample="";
+    var len=Math.floor(Math.random()*8+3);
+    for(i=0; i<len; i++)
+        {
+            sample+=charSet[Math.floor(Math.random()*charSet.length)];
         }
-        ,
-        templateUrl : "emailsEditor.html"
-        ,
-        scope: {
-            addEmail: "=addEmail",
-            getEmailsCount: "=getEmailsCount"
+    sample+="@";
+    len=Math.floor(Math.random()*8+3);
+    for(i=0; i<len; i++)
+        {
+            sample+=charSet[Math.floor(Math.random()*charSet.length)];
         }
-    };
-});
+    sample+=".";
+    len=Math.floor(Math.random()*2+2);
+    for(i=0; i<len; i++)
+        {
+            sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
+        }
+    return sample;
+}
 
 function addEmails($scope){
     var tmpEmails = $scope.emailsString.split(",");
