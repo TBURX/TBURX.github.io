96110bc6a3c6b30b768cb385e98502f14fc3a0c7
Vitaly <subbot.v@gmail.com>
2016-03-26 05:45:52 +0500

    buttons
    Вынес добавление рандомного мыла и к-во мыла в пачке из директивы.

--- emailsEditor.js
+++ emailsEditor.js
@@ -11,19 +11,10 @@ angular.module("editEmailsApp", [])
                 $scope.eMails = [];
                 $scope.pasted = false;
 
-                $scope.getEmailsCount = function(){
-                    alert("e-mails count is: " + $scope.eMails.length);
-                };
-
-                $scope.addEmail = function(){
-                    $scope.emailsString=genEmail();
-                    addEmails($scope);        
-                };
-
                 $scope.change = function(){
                     if($scope.emailsString.indexOf(",")>=0 || $scope.pasted)
                     {
-                        addEmails($scope);
+                        $scope.addEmails();
                         $scope.pasted = false;
                     }
                 };
@@ -31,12 +22,46 @@ angular.module("editEmailsApp", [])
                 $scope.keyUp = function($event){
                     if($event.keyCode==13)
                     {
-                        addEmails($scope);
+                        $scope.addEmails();
                     }
                 };
 
                 $scope.blur = function(){
-                    addEmails($scope);
+                    $scope.addEmails();
+                };
+                
+                $scope.addEmails = function(){
+                    
+                    var tmpEmails = $scope.emailsString.split(",");
+                    var i=0;
+                    while(i<tmpEmails.length)
+                        {
+                            tmpEmails[i]=tmpEmails[i].trim();
+                            if( tmpEmails[i]=="" || $scope.findIndexByName($scope.eMails,tmpEmails[i])>=0 )
+                                tmpEmails.splice(i,1);
+                            else
+                                i++;
+                        }
+
+                    var emailRegExp = /^[a-z]+[a-z0-9._-]+@([a-z0-9]+\.)+[a-z]{2,3}$/;
+                    for(i=0;i<tmpEmails.length;i++)
+                        {
+                            if(emailRegExp.test(tmpEmails[i]))
+                                $scope.eMails.push({name: tmpEmails[i], correct:"correct"});
+                            else
+                                $scope.eMails.push({name: tmpEmails[i], correct:"wrong"});
+                        }
+                    $scope.emailsString="";
+                };
+                
+                $scope.findIndexByName = function(source, name){
+                    for(i=0;i<source.length;i++){
+                        if(source[i].name === name)
+                            {
+                                return i;
+                            }
+                    }
+                    return -1;
                 };
                 
                 $scope.paste = function(){
@@ -55,70 +80,49 @@ angular.module("editEmailsApp", [])
             templateUrl : "emailsEditor.html"
             ,
             scope: {
-                addEmail: "=addEmail",
-                getEmailsCount: "=getEmailsCount",
-                emailsString: "@emailsString"
+                eMails: "=ngModel"
             }
         };
     })
+
+
+
     .controller("editEmailsController",function($scope){
-    })
-
-function genEmail(){    
-    var charSet="qwertyuiopasdfghjklzxcvbnm";
-    var sample="";
-    var len=Math.floor(Math.random()*8+3);
-    for(i=0; i<len; i++)
-        {
-            sample+=charSet[Math.floor(Math.random()*charSet.length)];
-        }
-    sample+="@";
-    len=Math.floor(Math.random()*8+3);
-    for(i=0; i<len; i++)
-        {
-            sample+=charSet[Math.floor(Math.random()*charSet.length)];
-        }
-    sample+=".";
-    len=Math.floor(Math.random()*2+2);
-    for(i=0; i<len; i++)
-        {
-            sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
-        }
-    return sample;
-}
-
-function addEmails($scope){
-    var tmpEmails = $scope.emailsString.split(",");
-    var i=0;
-    while(i<tmpEmails.length)
-        {
-            tmpEmails[i]=tmpEmails[i].trim();
-            if(tmpEmails[i]==""||
-               findIndexByName($scope.eMails,tmpEmails[i])>=0
-              )
-                tmpEmails.splice(i,1);
-            else
-                i++;
-        }
     
-    var emailRegExp = /^[a-z]+[a-z0-9._-]+@([a-z0-9]+\.)+[a-z.]{2,3}$/;
-    for(i=0;i<tmpEmails.length;i++)
-        {
-            if(emailRegExp.test(tmpEmails[i]))
-                $scope.eMails.push({name: tmpEmails[i], correct:"correct"});
-            else
-                $scope.eMails.push({name: tmpEmails[i], correct:"wrong"});
-        }
-    //$scope.eMails = $scope.eMails.concat(tmpEmails);
-    $scope.emailsString="";
-}
+        $scope.getEmailsCount = function(){
+            alert("e-mails count is: " + $scope.addresses.length);
+        };
+    
+        $scope.addEmail = function(){
+            $scope.addresses.push({ name:$scope.genEmail(), correct:"correct" });   
+        };
+    
+        $scope.genEmail = function(){    
+            var charSet="qwertyuiopasdfghjklzxcvbnm";
+            var sample="";
+            var len=Math.floor(Math.random()*8+3);
 
-function findIndexByName(source, name){
-    for(i=0;i<source.length;i++){
-        if(source[i].name === name)
-            {
-                return i;
-            }
-    }
-    return -1;
-}
+            for(i=0; i<len; i++)
+                {
+                    sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                }
+
+            sample+="@";
+
+            len=Math.floor(Math.random()*8+3);    
+            for(i=0; i<len; i++)
+                {
+                    sample+=charSet[Math.floor(Math.random()*charSet.length)];
+                }
+
+            sample+=".";
+
+            len=Math.floor(Math.random()*2+2);
+            for(i=0; i<len; i++)
+                {
+                    sample+=charSet[Math.floor(Math.random()*charSet.length)].toLowerCase();
+                }
+
+            return sample;
+        };
+    });
